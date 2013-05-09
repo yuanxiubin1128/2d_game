f6d8bb981e57ccb85fc577fa9639044730057155
Denes Matetelki <denes.matetelki@gmail.com>
2013-05-09 14:22:12 +0200

    First draft of files

---
+++ lib/subsystem_1/dummy_class.cpp
@@ -0,0 +1,17 @@
+#include "dummy_class.hpp"
+
+#include <iostream>
+
+namespace subsystem_1 {
+
+Dummy_class::Dummy_class()
+{
+  std::cout << "Dummy_class ctor" << std::endl;
+}
+
+Dummy_class::~Dummy_class()
+{
+  std::cout << "Dummy_class dtor" << std::endl;
+}
+
+} // namespace subsystem_1
---
+++ lib/subsystem_1/dummy_class.hpp
@@ -0,0 +1,15 @@
+#ifndef SUBSYSTEM_1_DUMMY_CLASS_HPP
+#define SUBSYSTEM_1_DUMMY_CLASS_HPP
+
+namespace subsystem_1 {
+
+  class Dummy_class {
+
+  public:
+    Dummy_class();
+    ~Dummy_class();
+  };
+
+}
+
+#endif // SUBSYSTEM_1_DUMMY_CLASS_HPP
