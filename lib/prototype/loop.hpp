dccafe287465f934569e02aa8cd177069fdfb7fd
Denes Matetelki <denes.matetelki@emerson.com>
2013-08-10 15:14:08 +0200

    Loop::run prints FPS and has internal slowDown with sleep

--- lib/prototype/loop.hpp
+++ lib/prototype/loop.hpp
@@ -6,6 +6,9 @@
 #include "widget.hpp"
 
 #include <cpp_utils/Thread.hpp>
+#include <cpp_utils/Logger.hpp>
+
+#include <ctime>
 
 namespace prototype
 {
@@ -17,22 +20,47 @@ public:
   Loop(Logic* l, Widget* w) : m_timer(), m_logic(l), m_widget(w) {}
 
 private:
-  void* run()
+  void* run() override
   {
     while(m_isRunning) {
 
       m_timer.registerStart();
 
-      double dt = m_timer.getAvaregeDelta();
+      double dt = m_timer.getAvaregeDelta() / 1000000.0;
       m_logic->step(dt);
-
       m_widget->render();
+
+      slowDown(0.01); // gives me ~100fps on a 6core xeon
+      logFps(dt);
 
       m_timer.registerStop();
     }
 
     return 0;
   }
+
+  void logFps(double delta) const
+  {
+    const double fps = double(1) / delta;
+    static int i;
+    ++i;
+    if (i == 100) {
+      LOG_BEGIN(Logger::INFO)
+        LOG_PROP("FPS: ", fps);
+      LOG_END("eh");
+      i = 0;
+    }
+  }
+
+  void slowDown(double s) const
+  {
+    const int nano = 1000000000;
+    struct timespec ts;
+    ts.tv_sec = 0;
+    ts.tv_nsec = s * nano;
+    nanosleep(&ts, 0);
+  }
+
 
   Timer<> m_timer;
   Logic* m_logic;
